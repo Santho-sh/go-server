e9cf529449578f9c6de49e39df6b17fdd56a1157
Santhosh <santho-sh@outlook.com>
2025-01-21 14:25:35 +0530

    add data structures and JSON handling for property listings

--- main.go
+++ main.go
@@ -1,16 +1,92 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
+
 	app := fiber.New()
+
+	type GeoLocation struct {
+		Longitude float64 `json:"longitude"`
+		Latitude  float64 `json:"latitude"`
+	}
+
+	type Address struct {
+		Street  string `json:"street"`
+		Area    string `json:"area"`
+		Pincode string `json:"pincode"`
+		City    string `json:"city"`
+		State   string `json:"state"`
+		Country string `json:"country"`
+	}
+
+	type Parking struct {
+		Available bool `json:"available"`
+	}
+
+	type Enquiry struct {
+		UserID    string    `json:"userId"`
+		UserName  string    `json:"userName"`
+		ListingID string    `json:"listingId"`
+		Number    string    `json:"number"`
+		CreatedAt time.Time `json:"createdAt"`
+	}
+
+	type Listing struct {
+		ID               string      `json:"id"`
+		Title            string      `json:"title"`
+		Description      string      `json:"description"`
+		Type             string      `json:"type"`
+		Location         GeoLocation `json:"location"`
+		ReadyToMove      bool        `json:"readyToMove"`
+		GatedCommunity   bool        `json:"gatedCommunity"`
+		LocationFeatures []string    `json:"locationFeatures"`
+		Amenities        []string    `json:"amenities"`
+		Price            int         `json:"price"`
+		Phone            string      `json:"phone"`
+		Address          Address     `json:"address"`
+		LandArea         int         `json:"landArea"`
+		Floors           int         `json:"floors"`
+		DTCPApproved     bool        `json:"dtcpApproved"`
+		Facing           string      `json:"facing"`
+		Parking          Parking     `json:"parking"`
+		Images           []string    `json:"images"`
+		Tags             []string    `json:"tags"`
+		CreatedAt        time.Time   `json:"createdAt"`
+		UpdatedAt        time.Time   `json:"updatedAt"`
+		Views            int         `json:"views"`
+		Contacted        int         `json:"contacted"`
+		Status           string      `json:"status"`
+		Enquiries        []Enquiry   `json:"enquiries"`
+	}
+
+	jsonFile, err := os.Open("data.json")
+
+	byteValue, _ := io.ReadAll(jsonFile)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var listings []Listing
+
+	json.Unmarshal(byteValue, &listings)
 
 	// Define a simple route
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World! Go app is running!")
+		return c.JSON(fiber.Map{
+			"message": "Hello, World! Go app is running!",
+			"data":    listings,
+		})
 	})
 
 	// CORS
@@ -22,5 +98,7 @@ func main() {
 	port := ":4000"
 	if err := app.Listen(port); err != nil {
 		panic(err)
+
 	}
+
 }
